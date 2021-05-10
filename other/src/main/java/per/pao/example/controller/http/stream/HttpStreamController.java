e0ef2cfbed512946db6654b028856d80a515b783
Pao-Sheng Wang <david840422@gmail.com>
2021-05-10 23:48:50 +0800

    spring boot server sent event

--- other/src/main/java/per/pao/example/controller/http/stream/HttpStreamController.java
+++ other/src/main/java/per/pao/example/controller/http/stream/HttpStreamController.java
@@ -3,10 +3,15 @@ package per.pao.example.controller.http.stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
 
 @RequiredArgsConstructor
 @RestController("httpStreamController")
@@ -37,5 +42,21 @@ public class HttpStreamController {
                     .contentType(MediaType.TEXT_PLAIN)
                     .body(responseBody);
         }
+    }
+
+    @GetMapping(value = "flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> flux() {
+        return Flux.interval(Duration.ofSeconds(3))
+                .map(sequence -> "Flux-" + LocalTime.now().toString() + " #" + sequence);
+    }
+
+    @GetMapping("sse")
+    public Flux<ServerSentEvent<String>> events() {
+        return Flux.interval(Duration.ofSeconds(3))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("periodic-event")
+                        .data("SSE - " + LocalTime.now().toString())
+                        .build());
     }
 }
