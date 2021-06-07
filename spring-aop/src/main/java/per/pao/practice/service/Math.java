02809b71d903748a83243162d26dfa58ce213484
Pao-Sheng Wang <david840422@gmail.com>
2021-06-07 22:37:26 +0800

    added slf4j

--- spring-aop/src/main/java/per/pao/practice/service/Math.java
+++ spring-aop/src/main/java/per/pao/practice/service/Math.java
@@ -1,22 +1,28 @@
 package per.pao.practice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class Math {
     public int add(int i1, int i2) {
+        log.info("do add method");
         return i1 + i2;
     }
 
     public int subtract(int i1, int i2) {
+        log.info("do subtract method");
         return i1 - i2;
     }
 
     public int multiply(int i1, int i2) {
+        log.info("do multiply method");
         return i1 * i2;
     }
 
     public int divide(int i1, int i2) {
+        log.info("do divide method");
         return i1 / i2;
     }
 }
