84cb2976cbed8d014f363b2958474faa8e0521dd
Pao-Sheng Wang <david840422@gmail.com>
2021-06-07 23:35:19 +0800

    added aop advice after

--- spring-aop/src/main/java/per/pao/practice/aop/before/LoggingAspect.java
+++ spring-aop/src/main/java/per/pao/practice/aop/before/LoggingAspect.java
@@ -3,10 +3,13 @@ package per.pao.practice.aop.before;
 import lombok.extern.slf4j.Slf4j;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Slf4j
 @Component
@@ -18,11 +21,17 @@ public class LoggingAspect {
         // do nothing
     }
 
-//    @Before("execution(* per.pao.practice.service.Math.add(..))")
+    //    @Before("execution(* per.pao.practice.service.Math.add(..))")
     @Before("log()")
     public void executeLog(JoinPoint jp) {
         String name = jp.getSignature().getName();
         log.info("before method {}", name);
+    }
+
+    @After("execution(* per.pao.practice.service.Math.subtract(..))")
+    public void executeLogAfter(JoinPoint jp) {
+        Arrays.stream(jp.getArgs())
+                .forEach(arg -> log.info("after method {} arg:{}", jp.getSignature().getName(), arg));
     }
 
 }
